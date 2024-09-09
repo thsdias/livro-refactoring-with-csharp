af6bd419ef74cd9123c38453753feaf7abc8fd1d
thsdias <thdias.84@gmail.com>
2024-09-09 20:43:11 -0300

    Chapter 5 - Beginning Code

---
+++ Chapter05/Chapter5/AirportInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Packt.CloudySkiesAir.Chapter5.AirTravel;
+
+public class Airport {
+  public string Country { get; set; }
+  public string Code { get; set; }
+  public string Name { get; set; }
+}
